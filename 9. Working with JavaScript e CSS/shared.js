5409a0bfed626560ec1e719107438f083f701de7
PaoloPci <paolopci@yahoo.it>
2024-10-17 11:51:03 +0200

    add close event side navigation bar

--- 9. Working with JavaScript e CSS/shared.js
+++ 9. Working with JavaScript e CSS/shared.js
@@ -4,6 +4,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 
 console.dir(selectPlanButtons);
@@ -15,8 +17,12 @@ for (i = 0; i < selectPlanButtons.length; i++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 modalNoButton.addEventListener('click', closeModal);
+toggleButton.addEventListener('click', openSideBar);
 
 // function modalView() {
 //     backdrop.style.display = 'block';
@@ -26,4 +32,9 @@ modalNoButton.addEventListener('click', closeModal);
 function closeModal() {
     backdrop.style.display = 'none';
     modal.style.display = 'none';
+}
+
+function openSideBar() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
 }
