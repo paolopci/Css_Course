64ef0b3a762887a0dca8cc92b780f8b48bac4795
PaoloPci <paolopci@yahoo.it>
2024-10-17 10:48:30 +0200

    var backdrop and var mal eventlisterner

--- 9. Working with JavaScript e CSS/shared.js
+++ 9. Working with JavaScript e CSS/shared.js
@@ -1,4 +1,24 @@
 
 
-var backdrop = document.querySelectorAll('.backdrop');
-console.dir(backdrop);
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+//console.dir(backdrop);
+
+//backdrop.style.display = 'block';
+
+var selectPlanButton = document.querySelectorAll('.plan button');
+console.dir(selectPlanButton);
+
+for (i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function () {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
+}
+
+
+
+// function modalView() {
+//     backdrop.style.display = 'block';
+//     modal.style.display = 'block';
+// }
