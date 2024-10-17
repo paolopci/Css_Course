d2c5501752d433c849d544a9cde098662571288b
PaoloPci <paolopci@yahoo.it>
2024-10-17 10:56:18 +0200

    add eventlistener for closeModal view

--- 9. Working with JavaScript e CSS/shared.js
+++ 9. Working with JavaScript e CSS/shared.js
@@ -2,23 +2,28 @@
 
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
-//console.dir(backdrop);
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-//backdrop.style.display = 'block';
 
-var selectPlanButton = document.querySelectorAll('.plan button');
-console.dir(selectPlanButton);
+console.dir(selectPlanButtons);
 
-for (i = 0; i < selectPlanButton.length; i++) {
-    selectPlanButton[i].addEventListener('click', function () {
+for (i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function () {
         backdrop.style.display = 'block';
         modal.style.display = 'block';
     });
 }
 
-
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
 
 // function modalView() {
 //     backdrop.style.display = 'block';
 //     modal.style.display = 'block';
 // }
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
